c9bf9b4872bd3f0ffe87ea562ac5b615c9b56eb1
EASK8ER52 <eask8er52@gmail.com>
2022-12-12 19:01:01 -0800

    Added files to my logger

--- src/main/java/com/revature/MainDriver.java
+++ src/main/java/com/revature/MainDriver.java
@@ -1,20 +1,39 @@
 package com.revature;
 
 
+import java.util.*;
+
 import com.revature.controller.RequestMapping;
 
 import io.javalin.Javalin;
 
 public class MainDriver
 {
+	public static List<String> names = new ArrayList<>();
 
 	public static void main(String[] args)
 	{
-		Javalin app = Javalin.create(confg ->
+		/*Javalin app = Javalin.create(confg ->
 		{
 			confg.plugins.enableDevLogging();
 		});
 		RequestMapping.setupEndpoints(app);
-		app.start(7000);
+		app.start(7000);*/
+		names.add("Clark");
+		names.add("Bruce");
+		names.add("Oliver");
+		Javalin app = Javalin.create();
+
+		app.get("/hello", ctx -> ctx.result("Hello Heroes!"));
+
+		app.post("/add", ctx ->
+		{
+			String newName = ctx.body();
+			names.add(newName);
+			ctx.result("Congrats you've added a new name to the database");
+			ctx.status(201);
+		});
+
+		app.start();
 	}
 }
