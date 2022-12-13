98be9889755b7b88f745f71935f9b2dbc6ef9fe9
EASK8ER52 <eask8er52@gmail.com>
2022-12-13 09:00:42 -0800

    Added some code for delete planet and a main to test

--- src/main/java/com/revature/repository/PlanetDao.java
+++ src/main/java/com/revature/repository/PlanetDao.java
@@ -1,32 +1,83 @@
 package com.revature.repository;
 
-import java.util.List;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.*;
 
 import com.revature.models.Planet;
+import com.revature.utilities.ConnectionUtil;
 
-public class PlanetDao {
-    
-    public List<Planet> getAllPlanets() {
+public class PlanetDao
+{
+    public List<Planet> getAllPlanets() throws SQLException
+	{
+		try (Connection con = ConnectionUtil.createConnection())
+		{
+			String sql = "select * from planets";
+			Statement statement = con.createStatement();
+			ResultSet rs = statement.executeQuery(sql);
+			List<Planet> planets = new ArrayList<>();
+			while (rs.next())
+			{
+				Planet planet = new Planet();
+				planet.setId(rs.getInt(1));
+				planet.setName(rs.getString(2));
+				planet.setOwnerId(rs.getInt(3));
+				planets.add(planet);
+			}
+			return planets;
+		}
+	}
+
+	public Planet getPlanetByName(String owner, String planetName)
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public Planet getPlanetByName(String owner, String planetName) {
+	public Planet getPlanetById(String username, int planetId)
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public Planet getPlanetById(String username, int planetId) {
+	public Planet createPlanet(String username, Planet p)
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public Planet createPlanet(String username, Planet p) {
-		// TODO Auto-generated method stub
-		return null;
+	public void deletePlanetById(int planetId)
+	{
+		try(Connection con = ConnectionUtil.createConnection())
+		{
+			String sql = "delete from planets where id = ?";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1, planetId);
+			int rowsAffected = ps.executeUpdate();
+			System.out.println("Rows affected" + rowsAffected);
+		}
+		catch(SQLException e)
+		{
+			System.out.println(e.getMessage());
+		}
 	}
 
-	public void deletePlanetById(int planetId) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		PlanetDao planetdao = new PlanetDao();
+		planetdao.deletePlanetById(2);
+		try
+		{
+			System.out.println(planetdao.getAllPlanets());
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e.getMessage());
+		}
 	}
 }
